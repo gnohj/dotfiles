bf1bc0b6a98402efcdb76635c674b7a6108173ca
gnohj <gnohj@users.noreply.github.com>
2024-11-24 13:44:09 -0600

    Latest

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -50,7 +50,7 @@ vim.keymap.set("n", "<C-a>", ":Neotree reveal<CR>", { desc = "Reveal active file
 
 -- Quickscope --
 -- vim.cmd([[
---   highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+--   highlight QuickScopePrimary ='#afff5f' gui=underline ctermfg=155 cterm=underline
 --   highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
 -- ]])
 vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
