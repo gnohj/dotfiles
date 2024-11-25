92afc9a0ad948637a1b13715477cb907fe5cac3e
gnohj <gnohj@users.noreply.github.com>
2024-11-24 19:14:06 -0600

    Update aerospace (janky borders); sketchybar theme

--- dot_config/sketchybar/items/front_apps.lua
+++ dot_config/sketchybar/items/front_apps.lua
@@ -6,75 +6,73 @@ local frontApps = {}
 sbar.add("bracket", constants.items.FRONT_APPS, {}, { position = "left" })
 
 local frontAppWatcher = sbar.add("item", {
-  drawing = false,
-  updates = true,
+	drawing = false,
+	updates = true,
 })
 
 local function selectFocusedWindow(frontAppName)
 	-- print("frontAppz", frontAppName)
-  for appName, app in pairs(frontApps) do
-    local isSelected = appName == frontAppName
-    local color = isSelected and settings.colors.orange or settings.colors.white
-    app:set(
-      {
-        label = { color = color },
-        icon = { color = color },
-      }
-    )
-  end
+	for appName, app in pairs(frontApps) do
+		local isSelected = appName == frontAppName
+		local color = isSelected and settings.colors.light_green or settings.colors.grey
+		app:set({
+			label = { color = color },
+			icon = { color = color },
+		})
+	end
 end
 
 local function updateWindows(windows)
-  sbar.remove("/" .. constants.items.FRONT_APPS .. "\\.*/")
+	sbar.remove("/" .. constants.items.FRONT_APPS .. "\\.*/")
 
-  frontApps = {}
-  local foundWindows = string.gmatch(windows, "[^\n]+")
-  for window in foundWindows do
-    local parsedWindow = {}
-    for key, value in string.gmatch(window, "(%w+)=([%w%s]+)") do
-      parsedWindow[key] = value
-    end
+	frontApps = {}
+	local foundWindows = string.gmatch(windows, "[^\n]+")
+	for window in foundWindows do
+		local parsedWindow = {}
+		for key, value in string.gmatch(window, "(%w+)=([%w%s]+)") do
+			parsedWindow[key] = value
+		end
 
-    local windowId = parsedWindow["id"]
-    -- print("windowId", windowId)
-    local windowName = parsedWindow["name"]
-    -- print("windowNAme", windowName);
-    local icon = settings.icons.apps[windowName] or settings.icons.apps["default"]
-    -- print("iconz", icon)
+		local windowId = parsedWindow["id"]
+		-- print("windowId", windowId)
+		local windowName = parsedWindow["name"]
+		-- print("windowNAme", windowName);
+		local icon = settings.icons.apps[windowName] or settings.icons.apps["default"]
+		-- print("iconz", icon)
 
-    -- Fetch workspace name and then add the frontApp item
-    sbar.exec(constants.aerospace.GET_CURRENT_WORKSPACE, function(workspaceName)
-      local labelString = windowName .. " | " .. workspaceName 
-      
-      frontApps[windowName] = sbar.add("item", constants.items.FRONT_APPS .. "." .. windowName, {
-        label = {
-          padding_left = 0,
-          string = labelString,  -- Interpolating windowName and workspaceName
-        },
-        icon = {
-          string = icon,
-          font = settings.fonts.icons(),
-        },
-        click_script = "aerospace focus --window-id " .. windowId,
-      })
+		-- Fetch workspace name and then add the frontApp item
+		sbar.exec(constants.aerospace.GET_CURRENT_WORKSPACE, function(workspaceName)
+			local labelString = windowName .. " | " .. workspaceName
 
-      frontApps[windowName]:subscribe(constants.events.FRONT_APP_SWITCHED, function(env)
-        selectFocusedWindow(env.INFO)
-      end)
-    end)
-  end
+			frontApps[windowName] = sbar.add("item", constants.items.FRONT_APPS .. "." .. windowName, {
+				label = {
+					padding_left = 0,
+					string = labelString, -- Interpolating windowName and workspaceName
+				},
+				icon = {
+					string = icon,
+					font = settings.fonts.icons(),
+				},
+				click_script = "aerospace focus --window-id " .. windowId,
+			})
 
-  sbar.exec(constants.aerospace.GET_CURRENT_WINDOW, function(frontAppName)
-    selectFocusedWindow(frontAppName:gsub("[\n\r]", ""))
-  end)
+			frontApps[windowName]:subscribe(constants.events.FRONT_APP_SWITCHED, function(env)
+				selectFocusedWindow(env.INFO)
+			end)
+		end)
+	end
+
+	sbar.exec(constants.aerospace.GET_CURRENT_WINDOW, function(frontAppName)
+		selectFocusedWindow(frontAppName:gsub("[\n\r]", ""))
+	end)
 end
 
 local function getWindows()
-  sbar.exec(constants.aerospace.LIST_WINDOWS, updateWindows)
+	sbar.exec(constants.aerospace.LIST_WINDOWS, updateWindows)
 end
 
 frontAppWatcher:subscribe(constants.events.UPDATE_WINDOWS, function()
-  getWindows()
+	getWindows()
 end)
 
 getWindows()
--- dot_config/sketchybar/items/widgets/calendar.lua
+++ dot_config/sketchybar/items/widgets/calendar.lua
@@ -1,17 +1,22 @@
 local constants = require("constants")
+local settings = require("config.settings")
 
 local calendar = sbar.add("item", constants.items.CALENDAR, {
-  position = "right",
-  update_freq = 1,
-  icon = { padding_left = 0, padding_right = 0 }
+	position = "right",
+	update_freq = 1,
+	icon = { padding_left = 0, padding_right = 0 },
+	color = settings.colors.light_blue,
 })
 
 calendar:subscribe({ "forced", "routine", "system_woke" }, function(env)
-  calendar:set({
-    label = os.date("%a %d %b, %H:%M"),
-  })
+	calendar:set({
+		label = {
+			string = os.date("%a %d %b, %H:%M"),
+			color = settings.colors.light_green,
+		},
+	})
 end)
 
 calendar:subscribe("mouse.clicked", function(env)
-  sbar.exec("open -a 'Calendar'")
+	sbar.exec("open -a 'Calendar'")
 end)
