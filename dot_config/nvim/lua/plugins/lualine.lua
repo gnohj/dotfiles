7794b7367e23d3687283c8fcb8e1a16eb5f01011
gnohj <gnohj@users.noreply.github.com>
2024-12-09 10:14:29 -0600

    add gitsigns to lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -98,6 +98,24 @@ return {
           },
           { "encoding" },
           { "filetype" },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
         },
       },
       winbar = {
