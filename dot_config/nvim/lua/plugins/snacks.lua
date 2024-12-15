5255a1035047501ec845a38ca4f96f646ae558d9
gnohj <gnohj@users.noreply.github.com>
2024-12-14 19:12:38 -0600

    updates

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -10,6 +10,17 @@ return {
     -- Configure the dashboard
     opts.dashboard = {
       preset = {
+        keys = {
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
         header = [[
                                                                     
       ████ ██████           █████      ██                     
