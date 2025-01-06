6a53e96143ffee931386966f26eeb534053306d3
gnohj <gnohj@users.noreply.github.com>
2025-01-06 16:38:45 -0600

    updates

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -6,6 +6,47 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      servers = {
+        tsserver = {
+          enabled = false,
+        },
+        ts_ls = {
+          enabled = false,
+        },
+        -- https://github.com/yioneko/vtsls
+        vtsls = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          settings = {
+            typescript = {
+              updateImportsOnFileMove = "always",
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              tsserver = {
+                maxTsServerMemory = 8192,
+              },
+              inlayHints = {
+                enumMemberValues = true,
+                functionLikeReturnTypes = true,
+                parameterNames = "literals",
+                parameterTypes = true,
+                propertyDeclarationTypes = true,
+                variableTypes = false,
+              },
+              preferences = {
+                includePackageJsonAutoImports = "off",
+              },
+            },
+          },
+        },
+      },
       inlay_hints = {
         enabled = false,
         exclude = {}, -- filetypes for which you don't want to enable inlay hints
