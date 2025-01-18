bb9764661d2f355b732aafd574cd45eabda9b53f
gnohj <gnohj@users.noreply.github.com>
2025-01-18 15:07:50 -0600

    updates

---
+++ dot_config/nvim/lua/plugins/bullets.lua
@@ -0,0 +1,11 @@
+-- https://github.com/bullets-vim/bullets.vim
+if vim.g.vscode then
+  return {}
+end
+
+return {
+  "bullets-vim/bullets.vim",
+  config = function()
+    vim.g.bullets_delete_last_bullet_if_empty = 1
+  end,
+}
---
+++ dot_config/nvim/lua/plugins/ghostty.lua
@@ -0,0 +1,10 @@
+if vim.g.vscode then
+  return {}
+end
+
+-- https://discord.com/channels/1005603569187160125/1300462095946485790/1300534513788653630
+return {
+  "ghostty",
+  dir = "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/",
+  lazy = false,
+}
