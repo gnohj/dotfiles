b9ed3007b799165ea4160438aa51bcdd8fbe7db7
gnohj <gnohj@users.noreply.github.com>
2025-01-20 17:08:12 -0600

    updates

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -5,15 +5,15 @@ end
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   enabled = true,
-  init = function()
+  opts = function()
     -- Define color variables
-    local color1_bg = "#5b4996"
-    local color2_bg = "#21925b"
-    local color3_bg = "#027d95"
-    local color4_bg = "#585c89"
-    local color5_bg = "#0f857c"
-    local color6_bg = "#396592"
-    local color_fg = "#0D1116"
+    local color1_bg = "#f265b5"
+    local color2_bg = "#37f499"
+    local color3_bg = "#04d1f9"
+    local color4_bg = "#a48cf2"
+    local color5_bg = "#f1fc79"
+    local color6_bg = "#f7c67f"
+    local color_fg = "#323449"
 
     -- Heading colors (when not hovered over), extends through the entire line
     vim.cmd(string.format([[highlight Headline1Bg guifg=%s guibg=%s]], color_fg, color1_bg))
@@ -23,77 +23,69 @@ return {
     vim.cmd(string.format([[highlight Headline5Bg guifg=%s guibg=%s]], color_fg, color5_bg))
     vim.cmd(string.format([[highlight Headline6Bg guifg=%s guibg=%s]], color_fg, color6_bg))
 
-    -- Highlight for the heading and sign icons (symbol on the left)
-    -- I have the sign disabled for now, so this makes no effect
-    vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg))
-    vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg))
-    vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg))
-    vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg))
-    vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg))
-    vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg))
+    return {
+      bullet = {
+        enabled = true,
+        right_pad = 1,
+      },
+      sign = {
+        -- Turn on / off sign rendering
+        enabled = true,
+        -- Applies to background of sign text
+        highlight = "RenderMarkdownSign",
+      },
+      checkbox = {
+        -- Turn on / off checkbox state rendering
+        enabled = true,
+        -- Determines how icons fill the available space:
+        --  inline:  underlying text is concealed resulting in a left aligned icon
+        --  overlay: result is left padded with spaces to hide any additional text
+        position = "inline",
+        unchecked = {
+          -- Replaces '[ ]' of 'task_list_marker_unchecked'
+          icon = "   󰄱 ",
+          -- Highlight for the unchecked icon
+          highlight = "RenderMarkdownUnchecked",
+          -- Highlight for item associated with unchecked checkbox
+          scope_highlight = nil,
+        },
+        checked = {
+          -- Replaces '[x]' of 'task_list_marker_checked'
+          icon = "   󰱒 ",
+          -- Highlight for the checked icon
+          highlight = "RenderMarkdownChecked",
+          -- Highlight for item associated with checked checkbox
+          scope_highlight = nil,
+        },
+      },
+      html = {
+        -- Turn on / off all HTML rendering
+        enabled = true,
+        comment = {
+          -- Turn on / off HTML comment concealing
+          conceal = false,
+        },
+      },
+      heading = {
+        sign = false,
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+        backgrounds = {
+          "Headline1Bg",
+          "Headline2Bg",
+          "Headline3Bg",
+          "Headline4Bg",
+          "Headline5Bg",
+          "Headline6Bg",
+        },
+        foregrounds = {
+          "Headline1Fg",
+          "Headline2Fg",
+          "Headline3Fg",
+          "Headline4Fg",
+          "Headline5Fg",
+          "Headline6Fg",
+        },
+      },
+    }
   end,
-  opts = {
-    bullet = {
-      enabled = true,
-      right_pad = 1,
-    },
-    sign = {
-      -- Turn on / off sign rendering
-      enabled = true,
-      -- Applies to background of sign text
-      highlight = "RenderMarkdownSign",
-    },
-    checkbox = {
-      -- Turn on / off checkbox state rendering
-      enabled = true,
-      -- Determines how icons fill the available space:
-      --  inline:  underlying text is concealed resulting in a left aligned icon
-      --  overlay: result is left padded with spaces to hide any additional text
-      position = "inline",
-      unchecked = {
-        -- Replaces '[ ]' of 'task_list_marker_unchecked'
-        icon = "   󰄱 ",
-        -- Highlight for the unchecked icon
-        highlight = "RenderMarkdownUnchecked",
-        -- Highlight for item associated with unchecked checkbox
-        scope_highlight = nil,
-      },
-      checked = {
-        -- Replaces '[x]' of 'task_list_marker_checked'
-        icon = "   󰱒 ",
-        -- Highlight for the checked icon
-        highlight = "RenderMarkdownChecked",
-        -- Highlight for item associated with checked checkbox
-        scope_highlight = nil,
-      },
-    },
-    html = {
-      -- Turn on / off all HTML rendering
-      enabled = true,
-      comment = {
-        -- Turn on / off HTML comment concealing
-        conceal = false,
-      },
-    },
-    heading = {
-      sign = false,
-      icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
-      backgrounds = {
-        "Headline1Bg",
-        "Headline2Bg",
-        "Headline3Bg",
-        "Headline4Bg",
-        "Headline5Bg",
-        "Headline6Bg",
-      },
-      foregrounds = {
-        "Headline1Fg",
-        "Headline2Fg",
-        "Headline3Fg",
-        "Headline4Fg",
-        "Headline5Fg",
-        "Headline6Fg",
-      },
-    },
-  },
 }
