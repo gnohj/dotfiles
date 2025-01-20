e0b0c405efad543a1bc273571715a8ded706d9b3
gnohj <gnohj@users.noreply.github.com>
2025-01-20 17:26:20 -0600

    Add snipe plugin

---
+++ dot_config/nvim/lua/plugins/snipe.lua
@@ -0,0 +1,27 @@
+if vim.g.vscode then
+  return {}
+end
+
+-- https://github.com/leath-dub/snipe.nvim
+return {
+  "leath-dub/snipe.nvim",
+  keys = {
+    {
+      "<S-l>",
+      function()
+        require("snipe").open_buffer_menu()
+      end,
+      desc = "Open Snipe buffer menu",
+    },
+  },
+  config = function()
+    local snipe = require("snipe")
+    snipe.setup({
+      navigate = {
+        cancel_snipe = "<esc>",
+        close_buffer = "d",
+      },
+      sort = "default",
+    })
+  end,
+}
