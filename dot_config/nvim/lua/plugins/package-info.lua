e200cb57d46e5e67c29b3d3d67703b135f8cafdf
gnohj <gnohj@users.noreply.github.com>
2025-03-27 12:18:52 -0500

    fix package-info and updates

--- dot_config/nvim/lua/plugins/package-info.lua
+++ dot_config/nvim/lua/plugins/package-info.lua
@@ -15,8 +15,25 @@ return {
     hide_up_to_date = true,
   },
   config = function(_, opts)
-    require("package-info").setup(opts)
-    -- manually register them https://github.com/vuki656/package-info.nvim/issues/155
+    -- Save the original show function
+    local package_info = require("package-info")
+    package_info.setup(opts)
+    -- Save the original show function
+    local original_show = package_info.show
+    -- Override the show function to check for mini.files
+    package_info.show = function()
+      -- Check if we're in mini.files
+      if vim.b.mini_files ~= nil then
+        return -- Don't do anything in mini.files
+      end
+      -- Check if buffer is valid for package.json operations
+      if vim.bo.buftype ~= "" then
+        return -- Don't do anything for special buffers
+      end
+      -- Call the original function
+      return original_show()
+    end
+
     -- vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
     vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
   end,
