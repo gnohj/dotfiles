4f611e63a4d8dea8e6bf6e26de26b6725c91d6d7
gnohj <gnohj@users.noreply.github.com>
2025-05-22 19:27:36 -0500

    add outline nvim plugin

---
+++ dot_config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,21 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = { -- Example mapping to toggle outline
+    { "<leader>O", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    -- Your setup opts here
+    symbol_folding = {
+      -- Unfold entire symbol tree by default with false, otherwise enter a
+      -- number starting from 1
+      autofold_depth = false,
+      -- autofold_depth = 1,
+    },
+    outline_window = {
+      -- Percentage or integer of columns
+      width = 33,
+    },
+  },
+}
