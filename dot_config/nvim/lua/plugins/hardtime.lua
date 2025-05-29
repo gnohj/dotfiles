2ba927d814350dd477ce4c43460fc19d5d6f5f15
gnohj <gnohj@users.noreply.github.com>
2025-05-29 18:41:08 -0500

    add back hardtime

--- dot_config/nvim/lua/plugins/hardtime.lua
+++ dot_config/nvim/lua/plugins/hardtime.lua
@@ -4,9 +4,16 @@ end
 
 return {
   "m4xshen/hardtime.nvim",
-  enabled = false,
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  enabled = true,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  event = "BufEnter",
   opts = {
     disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+    restricted_keys = {
+      ["jk"] = false, -- Allow jk combination
+      ["kj"] = false, -- Allow kj combination (if you use this too)
+      ["j"] = false, -- Allow j key
+      ["k"] = false, -- Allow k key
+    },
   },
 }
