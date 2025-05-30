028d920c5b223cebdf44cd7eee504f6e31c9bbe3
gnohj <gnohj@users.noreply.github.com>
2025-05-30 11:17:32 -0500

    update render-markdown bullets

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -16,67 +16,72 @@ return {
     vim.cmd(string.format([[highlight Headline7Bg guifg=%s guibg=%s]], "#cba6f7", "#403b5a"))
 
     return {
-      -- bullet = {
-      --   enabled = true,
-      --   right_pad = 1,
-      -- },
-      -- sign = {
-      --   -- Turn on / off sign rendering
-      --   enabled = true,
-      --   -- Applies to background of sign text
-      --   highlight = "RenderMarkdownSign",
-      -- },
-      -- checkbox = {
-      --   -- Turn on / off checkbox state rendering
-      --   enabled = true,
-      --   -- Determines how icons fill the available space:
-      --   --  inline:  underlying text is concealed resulting in a left aligned icon
-      --   --  overlay: result is left padded with spaces to hide any additional text
-      --   position = "inline",
-      --   unchecked = {
-      --     -- Replaces '[ ]' of 'task_list_marker_unchecked'
-      --     icon = "   󰄱 ",
-      --     -- Highlight for the unchecked icon
-      --     highlight = "RenderMarkdownUnchecked",
-      --     -- Highlight for item associated with unchecked checkbox
-      --     scope_highlight = nil,
-      --   },
-      --   checked = {
-      --     -- Replaces '[x]' of 'task_list_marker_checked'
-      --     icon = "   󰱒 ",
-      --     -- Highlight for the checked icon
-      --     highlight = "RenderMarkdownChecked",
-      --     -- Highlight for item associated with checked checkbox
-      --     scope_highlight = nil,
-      --   },
-      -- },
-      -- html = {
-      --   -- Turn on / off all HTML rendering
-      --   enabled = true,
-      --   comment = {
-      --     -- Turn on / off HTML comment concealing
-      --     conceal = false,
-      --   },
-      -- },
+      bullet = {
+        enabled = true,
+        right_pad = 1,
+      },
+      checkbox = {
+        -- Turn on / off checkbox state rendering
+        enabled = true,
+        -- Determines how icons fill the available space:
+        --  inline:  underlying text is concealed resulting in a left aligned icon
+        --  overlay: result is left padded with spaces to hide any additional text
+        position = "inline",
+        unchecked = {
+          -- Replaces '[ ]' of 'task_list_marker_unchecked'
+          icon = "   󰄱 ",
+          -- Highlight for the unchecked icon
+          highlight = "RenderMarkdownUnchecked",
+          -- Highlight for item associated with unchecked checkbox
+          scope_highlight = nil,
+        },
+        checked = {
+          -- Replaces '[x]' of 'task_list_marker_checked'
+          icon = "   󰱒 ",
+          -- Highlight for the checked icon
+          highlight = "RenderMarkdownChecked",
+          -- Highlight for item associated with checked checkbox
+          scope_highlight = nil,
+        },
+      },
+      html = {
+        -- Turn on / off all HTML rendering
+        enabled = true,
+        comment = {
+          -- Turn on / off HTML comment concealing
+          conceal = false,
+        },
+      },
+      link = {
+        image = "󰥶 ",
+        custom = {
+          youtu = { pattern = "youtu%.be", icon = "󰗃 " },
+        },
+      },
       -- heading = {
       --   sign = false,
       --   icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
-      --   backgrounds = {
-      --     "Headline1Bg",
-      --     "Headline2Bg",
-      --     "Headline3Bg",
-      --     "Headline4Bg",
-      --     "Headline5Bg",
-      --     "Headline6Bg",
-      --   },
-      --   foregrounds = {
-      --     "Headline1Fg",
-      --     "Headline2Fg",
-      --     "Headline3Fg",
-      --     "Headline4Fg",
-      --     "Headline5Fg",
-      --     "Headline6Fg",
-      --   },
+      --   -- backgrounds = {
+      --   --   "Headline1Bg",
+      --   --   "Headline2Bg",
+      --   --   "Headline3Bg",
+      --   --   "Headline4Bg",
+      --   --   "Headline5Bg",
+      --   --   "Headline6Bg",
+      --   -- },
+      --   -- foregrounds = {
+      --   --   "Headline1Fg",
+      --   --   "Headline2Fg",
+      --   --   "Headline3Fg",
+      --   --   "Headline4Fg",
+      --   --   "Headline5Fg",
+      --   --   "Headline6Fg",
+      --   -- },
+      -- },
+      -- code = {
+      --   -- if I'm not using yabai, I cannot make the color of the codeblocks
+      --   -- transparent, so just disabling all rendering 😢
+      --   style = "none",
       -- },
     }
   end,
