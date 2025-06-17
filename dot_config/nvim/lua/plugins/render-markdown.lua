d85c6850dff5656708bdf7237706d5eed5da1b2a
gnohj <gnohj@users.noreply.github.com>
2025-06-17 11:06:17 -0500

    make markdown headers block width

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -16,6 +16,8 @@ return {
     local color_fg = colors["gnohj_color26"]
     print(colors)
     -- local color_sign = "#ebfafa"
+    -- set in ~/.config/colorscheme/colorscheme-vars.sh
+    -- set in ~/.config/nvim/init.lua
     if vim.g.md_heading_bg == "transparent" then
       -- Define color variables
       local color1_bg = colors["gnohj_color04"]
@@ -41,15 +43,6 @@ return {
       -- Define inline code highlight for markdown
       vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s guibg=%s]], colorInline_bg, color_fg))
       -- vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s]], colorInline_bg))
-
-      -- Highlight for the heading and sign icons (symbol on the left)
-      -- I have the sign disabled for now, so this makes no effect
-      vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg))
-      vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg))
-      vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg))
-      vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg))
-      vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg))
-      vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg))
     else
       local color1_bg = colors["gnohj_color18"]
       local color2_bg = colors["gnohj_color19"]
@@ -110,6 +103,7 @@ return {
       },
       heading = {
         sign = false,
+        width = "block",
         icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
         backgrounds = {
           "Headline1Bg",
