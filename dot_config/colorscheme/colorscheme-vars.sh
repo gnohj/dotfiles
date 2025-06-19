50d69907da59b471ec05ce11bd8e42d7d333bfe5
gnohj <gnohj@users.noreply.github.com>
2025-06-19 15:30:23 -0500

    update markdown headings

--- dot_config/colorscheme/colorscheme-vars.sh
+++ dot_config/colorscheme/colorscheme-vars.sh
@@ -5,5 +5,5 @@ colorscheme_profile="gnohj-colors.sh"
 
 # I have 2 Neovim markdown heading style options
 export THEME_TRANSPARENT=transparent
-export MD_HEADING_BG=transparent
-# export MD_HEADING_BG=solid
+# export MD_HEADING_BG=transparent
+export MD_HEADING_BG=solid
