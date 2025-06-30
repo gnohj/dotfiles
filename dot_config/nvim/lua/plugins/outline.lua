fec3e87aa4c2f22a08405b800f12a96975b7e787
gnohj <gnohj@users.noreply.github.com>
2025-06-30 11:56:40 -0500

    🔧 Start outline with everything folded upon opening

--- dot_config/nvim/lua/plugins/outline.lua
+++ dot_config/nvim/lua/plugins/outline.lua
@@ -12,10 +12,12 @@ return {
   opts = {
     -- Your setup opts here
     symbol_folding = {
-      -- Unfold entire symbol tree by default with false, otherwise enter a
-      -- number starting from 1
-      autofold_depth = false,
-      -- autofold_depth = 1,
+      -- Unfold entire symbol tree by default with false, otherwise enter 0 to fold when opening
+      autofold_depth = 0,
+      auto_unfold = {
+        only = true,
+        hovered = true,
+      },
     },
     outline_window = {
       -- Percentage or integer of columns
