56e57f8b9b1ea621f1e3b4acf4c8d16327efbbf0
gnohj <gnohj@users.noreply.github.com>
2025-07-09 06:14:27 -0500

    🔧 Add active buffer path to <leader>sr - Grugfar

--- dot_config/nvim/lua/plugins/grug-far.lua
+++ dot_config/nvim/lua/plugins/grug-far.lua
@@ -13,5 +13,31 @@ return {
       resultLocation = { showNumberLabel = false },
       -- showCompactInputs = true,
     },
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          local current_file = vim.api.nvim_buf_get_name(0)
+          local path
+
+          if current_file ~= "" then
+            -- Use current file's directory if buffer has a file
+            path = vim.fn.fnamemodify(current_file, ":h")
+          else
+            -- Fallback to current working directory
+            path = vim.fn.getcwd()
+          end
+
+          grug.open({
+            prefills = {
+              paths = path,
+            },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
+    },
   },
 }
