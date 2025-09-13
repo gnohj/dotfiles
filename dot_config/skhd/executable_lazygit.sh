e81a880f6a3db25ad559417ada37eb9b07fb784b
gnohj <gnohj@users.noreply.github.com>
2025-09-13 10:49:37 -0500

    🔧 Bypass husky by default when using lazygit

--- dot_config/skhd/executable_lazygit.sh
+++ dot_config/skhd/executable_lazygit.sh
@@ -6,6 +6,6 @@ export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin"
   PANE_CWD=$(/opt/homebrew/bin/tmux show-environment -t "#{pane_id}" PANE_CWD 2>/dev/null | cut -d= -f2)
   DIR="${PANE_NVIM_CWD:-${PANE_CWD:-#{pane_current_path}}}"
   /opt/homebrew/bin/tmux display-popup -E -w 90% -h 90% -d "$DIR" "
-    /usr/bin/git rev-parse --is-inside-work-tree >/dev/null 2>&1 && LG_CONFIG_FILE=~/.config/lazygit/config.yml /opt/homebrew/bin/lazygit || exit 0
+    /usr/bin/git rev-parse --is-inside-work-tree >/dev/null 2>&1 && HUSKY=0 LG_CONFIG_FILE=~/.config/lazygit/config.yml /opt/homebrew/bin/lazygit || exit 0
   "
 '
