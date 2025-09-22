7a83d2e51717bad2bed4532d59de6017107b9944
gnohj <gnohj@users.noreply.github.com>
2025-09-22 12:47:15 -0500

    🐛 Get rid of lsp eslint fix hack, fixed by lazyvim

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -26,9 +26,9 @@ return {
                 -- Limit workspace loading but don't disable it completely
                 library = {},
                 checkThirdParty = false,
-                maxPreload = 500,  -- Reasonable limit for Neovim config files
-                preloadFileSize = 50,  -- KB - only preload small files
-                ignoreDir = { ".git", "node_modules", "lazy" },  -- Ignore these directories
+                maxPreload = 500, -- Reasonable limit for Neovim config files
+                preloadFileSize = 50, -- KB - only preload small files
+                ignoreDir = { ".git", "node_modules", "lazy" }, -- Ignore these directories
                 ignoreSubmodules = true,
               },
               diagnostics = {
@@ -37,7 +37,7 @@ return {
                 disable = { "lowercase-global" },
               },
               completion = {
-                workspaceWord = false,  -- Don't scan workspace for completions
+                workspaceWord = false, -- Don't scan workspace for completions
                 showWord = "Disable",
               },
               -- Disable telemetry
@@ -56,9 +56,12 @@ return {
               return vim.fn.expand("~/.config/nvim")
             end
             -- For other lua files, look for .luarc.json or .git
-            return util.root_pattern(".luarc.json", ".luarc.jsonc", ".git")(fname)
-              or util.find_git_ancestor(fname)
-              or vim.fn.fnamemodify(fname, ":h")  -- Use file's directory as root
+            return util.root_pattern(".luarc.json", ".luarc.jsonc", ".git")(
+              fname
+            ) or util.find_git_ancestor(fname) or vim.fn.fnamemodify(
+              fname,
+              ":h"
+            ) -- Use file's directory as root
           end,
         },
         harper_ls = {
@@ -177,51 +180,51 @@ return {
 
       -- Add the eslint setup configuration - this is from https://github.com/iainlane/dotfiles/commit/1abe290bfe071b92a806eea62abadbab18ee63c3
       -- to fix eslint formatting which was broken in Neovim 0.11.0
-      opts.setup = opts.setup or {}
-      opts.setup.eslint = function()
-        local function get_client(buf)
-          return LazyVim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
-        end
-
-        local formatter = LazyVim.lsp.formatter({
-          name = "eslint: lsp",
-          primary = false,
-          priority = 200,
-          filter = "eslint",
-        })
-
-        -- Modified condition: For both Neovim < 0.10.0 and Neovim 0.11.0+
-        -- The original check was breaking in 0.11.0, so we handle both old versions
-        -- and the specific case of 0.11.0
-        if vim.fn.has("nvim-0.10") == 0 or vim.version().minor >= 11 then
-          formatter.name = "eslint: EslintFixAll"
-          formatter.sources = function(buf)
-            local client = get_client(buf)
-            return client and { "eslint" } or {}
-          end
-          formatter.format = function(buf)
-            local client = get_client(buf)
-            if client then
-              local pull_diagnostics = vim.diagnostic.get(buf, {
-                namespace = vim.lsp.diagnostic.get_namespace(client.id, false),
-              })
-              -- Older versions of the ESLint language server send push
-              -- diagnostics rather than using pull. We support both for
-              -- backwards compatibility.
-              local push_diagnostics = vim.diagnostic.get(buf, {
-                namespace = vim.lsp.diagnostic.get_namespace(client.id, true),
-              })
-              if (#pull_diagnostics + #push_diagnostics) > 0 then
-                vim.cmd("EslintFixAll")
-              end
-            end
-          end
-        end
-
-        -- register the formatter with LazyVim
-        LazyVim.format.register(formatter)
-      end
-
+      -- opts.setup = opts.setup or {}
+      -- opts.setup.eslint = function()
+      --   local function get_client(buf)
+      --     return LazyVim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
+      --   end
+      --
+      --   local formatter = LazyVim.lsp.formatter({
+      --     name = "eslint: lsp",
+      --     primary = false,
+      --     priority = 200,
+      --     filter = "eslint",
+      --   })
+      --
+      --   -- Modified condition: For both Neovim < 0.10.0 and Neovim 0.11.0+
+      --   -- The original check was breaking in 0.11.0, so we handle both old versions
+      --   -- and the specific case of 0.11.0
+      --   if vim.fn.has("nvim-0.10") == 0 or vim.version().minor >= 11 then
+      --     formatter.name = "eslint: EslintFixAll"
+      --     formatter.sources = function(buf)
+      --       local client = get_client(buf)
+      --       return client and { "eslint" } or {}
+      --     end
+      --     formatter.format = function(buf)
+      --       local client = get_client(buf)
+      --       if client then
+      --         local pull_diagnostics = vim.diagnostic.get(buf, {
+      --           namespace = vim.lsp.diagnostic.get_namespace(client.id, false),
+      --         })
+      --         -- Older versions of the ESLint language server send push
+      --         -- diagnostics rather than using pull. We support both for
+      --         -- backwards compatibility.
+      --         local push_diagnostics = vim.diagnostic.get(buf, {
+      --           namespace = vim.lsp.diagnostic.get_namespace(client.id, true),
+      --         })
+      --         if (#pull_diagnostics + #push_diagnostics) > 0 then
+      --           vim.cmd("EslintFixAll")
+      --         end
+      --       end
+      --     end
+      --   end
+      --
+      --   -- register the formatter with LazyVim
+      --   LazyVim.format.register(formatter)
+      -- end
+      --
       -- 🔒 disable <leader>ca keymap here (cleaner than on_attach override)
       -- local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- keys[#keys + 1] = { "<leader>ca", false }
