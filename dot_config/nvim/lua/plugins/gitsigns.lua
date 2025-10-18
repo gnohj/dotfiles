346ed2106889a9d9ab361833397c58c9cbe7ee35
gnohj <gnohj@users.noreply.github.com>
2025-10-18 11:52:14 -0500

    🔧 Update git signs - thicker emojis

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -3,12 +3,12 @@ return {
   event = "LazyFile",
   opts = {
     signs = {
-      add = { text = "·" },
-      change = { text = "·" },
-      delete = { text = "·" },
-      topdelete = { text = "·" },
-      changedelete = { text = "·" },
-      untracked = { text = "·" },
+      add = { text = "•" },
+      change = { text = "•" },
+      delete = { text = "" },
+      topdelete = { text = "•" },
+      changedelete = { text = "•" },
+      untracked = { text = "•" },
     },
     signs_staged = {
       add = { text = "·" },
