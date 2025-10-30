d99e18facc67ae27173f2c67a4e886aae9ec5677
gnohj <gnohj@users.noreply.github.com>
2025-10-30 13:43:44 -0500

    🔧 Update sesh; remove lazygit tmux border pop-up

--- dot_config/skhd/executable_lazygit.sh
+++ dot_config/skhd/executable_lazygit.sh
@@ -7,7 +7,7 @@ tmux run-shell '
   PANE_NVIM_CWD=$(tmux show-environment -t "#{pane_id}" NVIM_CWD_#{pane_id} 2>/dev/null | cut -d= -f2)
   PANE_CWD=$(tmux show-environment -t "#{pane_id}" PANE_CWD 2>/dev/null | cut -d= -f2)
   DIR="${PANE_NVIM_CWD:-${PANE_CWD:-#{pane_current_path}}}"
-  tmux display-popup -E -w 90% -h 90% -d "$DIR" "
+  tmux display-popup -E -w 90% -h 90% -d "$DIR" -B "
     eval \"\$(mise activate bash)\" && /usr/bin/git rev-parse --is-inside-work-tree >/dev/null 2>&1 && HUSKY=0 LG_CONFIG_FILE=~/.config/lazygit/config.yml lazygit || exit 0
   "
 '
