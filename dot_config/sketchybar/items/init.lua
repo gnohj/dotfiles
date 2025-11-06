541c163151132881cdad5622e8babea5a4e9fc39
gnohj <gnohj@users.noreply.github.com>
2025-11-06 15:24:21 -0600

    🔧 Add launchpad binding to skhd

--- dot_config/sketchybar/items/init.lua
+++ dot_config/sketchybar/items/init.lua
@@ -3,7 +3,7 @@
 require("items.workspace")
 
 -- Middle Items
--- require("items.spotify")  -- DISABLED - Even event-driven AppleScript causes freezes
+require("items.spotify") -- DISABLED - Even event-driven AppleScript causes freezes
 
 -- Right items
 require("items.widgets")
