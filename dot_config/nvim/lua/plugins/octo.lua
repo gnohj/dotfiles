6619526ca57f274f91c627cd8c5fa4f3634d2b96
gnohj <gnohj@users.noreply.github.com>
2026-01-10 13:50:28 -0500

    ✨ feat(nvim): add octo.nvim plugin for GitHub integration

---
+++ dot_config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,58 @@
+return {
+  "pwntester/octo.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "folke/snacks.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = { "Octo" },
+  opts = {
+    enable_builtin = true,
+    picker = "snacks",
+    mappings = {
+      review_diff = {
+        select_next_entry = {
+          lhs = "<Tab>",
+          desc = "move to previous changed file",
+        },
+        select_prev_entry = {
+          lhs = "<S-Tab>",
+          desc = "move to next changed file",
+        },
+        close_review_tab = {
+          lhs = "<esc>",
+          desc = "close review tab",
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("octo").setup(opts)
+
+    -- Add <esc> to close octo buffers (issue/PR views)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "octo",
+      callback = function(event)
+        vim.keymap.set("n", "<esc>", function()
+          -- Close window and delete buffer
+          local buf = vim.api.nvim_get_current_buf()
+          local wins = vim.fn.win_findbuf(buf)
+          for _, win in ipairs(wins) do
+            if vim.api.nvim_win_is_valid(win) then
+              vim.api.nvim_win_close(win, true)
+            end
+          end
+          if vim.api.nvim_buf_is_valid(buf) then
+            vim.api.nvim_buf_delete(buf, { force = true })
+          end
+        end, {
+          buffer = event.buf,
+          desc = "Close Octo buffer",
+        })
+      end,
+    })
+  end,
+  keys = {
+    { "<leader>oO", "<cmd>Octo<cr>", desc = "Octo" },
+  },
+}
