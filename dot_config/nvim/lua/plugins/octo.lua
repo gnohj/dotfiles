f8db1a1cd9191a50708cb18afafcc7e9d1f4e6c6
gnohj <gnohj@users.noreply.github.com>
2026-01-11 10:48:27 -0500

    ✨ feat(octo): add new keybindings for review actions

--- dot_config/nvim/lua/plugins/octo.lua
+++ dot_config/nvim/lua/plugins/octo.lua
@@ -13,15 +13,41 @@ return {
       review_diff = {
         select_next_entry = {
           lhs = "<Tab>",
-          desc = "move to previous changed file",
+          desc = "move to next changed file",
         },
         select_prev_entry = {
           lhs = "<S-Tab>",
-          desc = "move to next changed file",
+          desc = "move to previous changed file",
         },
         close_review_tab = {
           lhs = "<esc>",
           desc = "close review tab",
+        },
+        add_review_comment = {
+          lhs = "<leader>ca",
+          desc = "add review comment",
+          mode = { "n", "x" },
+        },
+        add_review_suggestion = {
+          lhs = "<leader>sa",
+          desc = "add review suggestion",
+          mode = { "n", "x" },
+        },
+        next_thread = {
+          lhs = "]t",
+          desc = "next comment thread",
+        },
+        prev_thread = {
+          lhs = "[t",
+          desc = "previous comment thread",
+        },
+        submit_review = {
+          lhs = "<leader>vs",
+          desc = "submit review",
+        },
+        discard_review = {
+          lhs = "<leader>vd",
+          desc = "discard review",
         },
       },
     },
