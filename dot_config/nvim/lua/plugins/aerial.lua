db348474becc579c93daa040fcdc22d7e18579de
gnohj <gnohj@users.noreply.github.com>
2026-01-21 08:36:20 -0500

    ♻️ refactor(aerial): remove autocmd for closing aerial on escape

--- dot_config/nvim/lua/plugins/aerial.lua
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -7,17 +7,5 @@ return {
   opts = {},
   config = function(_, opts)
     require("aerial").setup(opts)
-
-    -- Set up escape to close aerial
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "aerial",
-      callback = function(event)
-        vim.keymap.set("n", "<esc>", "<cmd>AerialClose<cr>", {
-          buffer = event.buf,
-          silent = true,
-          desc = "Close Aerial",
-        })
-      end,
-    })
   end,
 }
