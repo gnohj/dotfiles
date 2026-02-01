2d66edcb3d3ff7c22f3e74fa6b2395b0510484fb
gnohj <gnohj@users.noreply.github.com>
2026-02-01 15:39:15 -0500

    ✨ feat(which-key): add new key groupings and descriptions

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -7,7 +7,19 @@ return {
   end,
   opts = {
     spec = {
-      { "<leader>gh", group = false },
+      { "<leader>gh", hidden = true },
+      { "<leader>gL", hidden = true },
+      { "<leader>G", hidden = true },
+      { "<leader>K", hidden = true },
+      { "<leader>L", hidden = true },
+      { "<leader>n", hidden = true },
+      { "<leader>a", group = "toggle copilot suggestions" },
+      { "<leader>m", group = "toggle c" },
+      { "<leader>o", group = "opencode" },
+      { "<leader>z", group = "obsidian" },
+      { "<leader>h", group = "hunks" },
+      { "<leader>p", group = "yank history" },
+      { "<leader>-", desc = "Open yazi at cwd" },
     },
   },
 }
