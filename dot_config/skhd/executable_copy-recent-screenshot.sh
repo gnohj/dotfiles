8f0928ff26d932cd6590d84fb31fd5de75d8a2be
gnohj <gnohj@users.noreply.github.com>
2026-02-12 11:38:39 -0600

    ♻️ refactor(scripts): update screenshot scripts to copy filepaths instead of images

--- dot_config/skhd/executable_copy-recent-screenshot.sh
+++ dot_config/skhd/executable_copy-recent-screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copy the most recent screenshot image to clipboard (paste directly into apps)
+# Copy the most recent screenshot filepath to clipboard
 
 SCREENSHOT_DIR="$HOME/Pictures"
 
@@ -10,13 +10,13 @@ RECENT_FILE=$(fd -t f '^(SCR-|Screenshot-)' "$SCREENSHOT_DIR" -x stat -f "%m %N"
   cut -d' ' -f2-)
 
 if [ -z "$RECENT_FILE" ]; then
-  osascript -e 'display notification "No recent screenshot found" with title "Copy Screenshot"'
+  osascript -e 'display notification "No recent screenshot found" with title "Copy Screenshot Path"'
   exit 1
 fi
 
-# Copy image to clipboard with clippy (paste directly into apps)
-clippy "$RECENT_FILE"
+# Copy filepath to clipboard
+echo -n "$RECENT_FILE" | pbcopy
 
 # Show notification
 FILENAME=$(basename "$RECENT_FILE")
-osascript -e "display notification \"$FILENAME\" with title \"Screenshot copied to clipboard!\""
+osascript -e "display notification \"$FILENAME\" with title \"Screenshot path copied!\""
--- dot_config/skhd/executable_screenshot-save-and-copy.sh
+++ dot_config/skhd/executable_screenshot-save-and-copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Native macOS screenshot - save to file AND copy image to clipboard
+# Native macOS screenshot - save to file AND copy filepath to clipboard
 
 # Generate filename with timestamp
 FILENAME="Screenshot-$(date +%Y%m%d-%H%M%S).png"
@@ -8,7 +8,7 @@ FILEPATH="$HOME/Pictures/$FILENAME"
 # Take screenshot and save to file
 screencapture -i "$FILEPATH"
 
-# If file was created, copy image to clipboard with clippy (paste directly into apps)
+# If file was created, copy filepath to clipboard
 if [ -f "$FILEPATH" ]; then
-    clippy "$FILEPATH"
+    echo -n "$FILEPATH" | pbcopy
 fi
