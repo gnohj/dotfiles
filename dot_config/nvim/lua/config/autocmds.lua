d26be878e070feb5e66d0fdb806dd96fed1afb39
gnohj <gnohj@users.noreply.github.com>
2026-04-25 18:16:53 -0400

    🐛 fix(autocmds): update markdown autocmd to match FileType

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -20,7 +20,7 @@ end
 -- Wrap for markdown files
 -- ============================================================================
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*.md",
+  pattern = "markdown",
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
