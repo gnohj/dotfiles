e2b3156f17fbabb3508a85dfe07ffcbd58f14f57
gnohj <gnohj@users.noreply.github.com>
2026-04-26 13:27:37 -0400

    ✨ feat(markdown): support image pasting with obsidian-style paths

---
+++ dot_config/nvim/lua/plugins/img-clip.lua
@@ -0,0 +1,42 @@
+-- Paste images from clipboard into markdown notes
+-- https://github.com/HakonHarnes/img-clip.nvim
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {
+    default = {
+      embed_image_as_base64 = false,
+      prompt_for_file_name = true,
+      drag_and_drop = {
+        insert_mode = true,
+      },
+      use_absolute_path = false,
+    },
+    filetypes = {
+      markdown = {
+        url_encode_path = true,
+        template = "![[$FILE_NAME]]",
+        download_images = false,
+        -- Obsidian-style: paste into vault Assets/ regardless of cwd
+        dir_path = function()
+          local vault = vim.fn.expand("~/Obsidian/second-brain")
+          local current = vim.fn.expand("%:p")
+          if current:find(vault, 1, true) then
+            return vault .. "/Assets"
+          end
+          return "assets"
+        end,
+        file_name = "%y%m%d-%H%M%S",
+        relative_to_current_file = false,
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>zv",
+      "<cmd>PasteImage<cr>",
+      desc = "[P]Obsidian: Paste image from clipboard",
+      mode = { "n", "i" },
+    },
+  },
+}
